9b12c782ce8591a9ee7c0c0d0c0ce53c65637d11
Alessio Iardino <ale.iar@hotmail.com>
2023-08-30 17:21:37 +0200

    create repository and mongodb context

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext( IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DataSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DataSettings:ProductDb"));
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DataSettings:Products"));
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
